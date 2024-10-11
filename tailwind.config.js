d88526ee67534cfcc4d1f34ca918679d3647263f
ykkhmrdn <ykkhmrdn@gmail.com>
2024-10-11 20:48:26 +0700

    Refactor tailwind.config.js to use ES modules syntax

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,11 @@
-module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-      gradientColorStops: (theme) => ({
-        primary: "#6366F1",
-        secondary: "#8B5CF6",
-      }),
-    },
+export const content = ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"];
+export const theme = {
+  extend: {
+    // eslint-disable-next-line no-unused-vars
+    gradientColorStops: (theme) => ({
+      primary: "#6366F1",
+      secondary: "#8B5CF6",
+    }),
   },
-  plugins: [],
 };
+export const plugins = [];
